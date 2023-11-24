3d8d9fc00ffc240459675b1c62a2d8a31fad8cdf
Edward Coughlan <ecoughlan@letsgetchecked.com>
2023-11-24 10:12:38 +0000

    Add stories and add story generation to the library creation

--- create-react-library.mjs
+++ create-react-library.mjs
@@ -11,3 +11,5 @@ const parseArgumentsIntoOptions = (rawArgs) => {
 
 const { name } = parseArgumentsIntoOptions(process.argv.splice(2));
 execSync(`npx nx generate @nx/react:library --name=${name} --bundler=vite --directory=packages/react/ --compiler=swc --importPath=@edwardcoughlan/design-system-${name} --projectNameAndRootFormat=derived --simpleName=true --no-interactive `, { stdio: "inherit" })
+execSync(`npx nx generate @nx/react:stories --project=${name} --no-interactive`, { stdio: "inherit" })
+
