65739a90473b925bfac63aab16815f122173839e
Edward Coughlan <ecoughlan@letsgetchecked.com>
2023-11-24 01:33:53 +0000

    😱

--- packages/react/button/src/lib/button.tsx
+++ packages/react/button/src/lib/button.tsx
@@ -3,7 +3,7 @@ export interface ButtonProps {}
 
 export function Button(props: ButtonProps) {
   return (
-    <div className={'bg-black text-white text-heading-sm my-50 text'}>
+    <div className={'bg-black text-white text-heading-sm my-50'}>
       <h1>Welcome to Button!</h1>
       <div className="p-50">hello</div>
     </div>
